e06ab71c016a5c978de1dfd80bb35daded37a210
wilsongit <wilsonwilas.123@gmail.com>
2026-02-21 20:29:36 +0100

    first  commmit

---
+++ script.js
@@ -0,0 +1,10 @@
+  function showsidebar(){
+    const sidebar =document.querySelector('.sidebar')
+    sidebar .style.display ='flex'
+
+  }
+
+  function hidesidebar(){
+    const sidebar =document.querySelector('.sidebar')
+    sidebar .style.display ='none'
+  } 
